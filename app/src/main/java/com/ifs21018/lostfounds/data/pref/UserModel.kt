0c66b85d6187530c3ed4b68ca92a3eb706ba4cc4
syalomadv <syalommanurung123@gmail.com>
2024-04-29 00:43:18 +0700

    first commit

---
+++ app/src/main/java/com/ifs21018/lostfounds/data/pref/UserModel.kt
@@ -0,0 +1,6 @@
+package com.ifs21018.lostfounds.data.pref
+
+data class UserModel(
+    val token: String,
+    val isLogin: Boolean = false
+)
---
+++ app/src/main/java/com/ifs21018/lostfounds/data/remote/response/DelcomResponse.kt
@@ -0,0 +1,12 @@
+package com.ifs21018.lostfounds.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class DelcomResponse(
+
+	@field:SerializedName("success")
+	val success: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
